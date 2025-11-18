56c85aa7562c18b2aaad6fd54a18baf41a60af58
Amsa <amishagurung38@gmail.com>
2025-11-18 20:28:06 +0545

    Rating API using Query numerical validation

--- day-04/query.py
+++ day-04/query.py
@@ -14,3 +14,9 @@ def get_age(age: int = Query(..., ge=1, le=120)):
 @app.get("/search")
 def get_query(query: str = Query(..., min_length=3, max_length=50)):
     return {"query": query}
+
+#rating API
+@app.get("/rate")
+def rate(value: int = Query(..., ge=1, le=5)):
+    return {"rating": value}
+
