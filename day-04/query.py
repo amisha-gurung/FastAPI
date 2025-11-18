65fe31e8177e4aef5df1205c1f6d4f4ad4369784
Amsa <amishagurung38@gmail.com>
2025-11-18 18:54:46 +0545

    String validation with Query

--- day-04/query.py
+++ day-04/query.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI, Query
 
 app = FastAPI()
 
+#numerical validation:
 @app.get("/check-age")
 def get_age(age: int = Query(..., ge=1, le=120)):
     if age >= 18:
         return {"Status": "Adult"}
     else:
         return {"Status": "Minor"}
-    
+
+#string validation:
+@app.get("/search")
+def get_query(query: str = Query(..., min_length=3, max_length=50)):
+    return {"query": query}
